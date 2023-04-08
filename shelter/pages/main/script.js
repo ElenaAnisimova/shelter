51ef2f0cc834ab0e705070daabc8aaea83d2e7c1
elenaanisimova <anilena232@mail.ru~>
2023-04-08 23:41:32 +0800

    feat: add slider features

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -45,9 +45,9 @@ import { petsDescription } from "../../assets/pet-info.js"
 // console.log(petsDescription[6].name)
 
 const slider = document.querySelector('.slider-visible-wrapper')
-const sliderPartOne = document.querySelector('.slider-part-one');
-const sliderPartTwo = document.querySelector('.slider-part-two');
-const sliderPartThree = document.querySelector('.slider-part-three');
+const sliderPartOne = document.querySelector('.slider-part-zero');
+const sliderPartTwo = document.querySelector('.slider-part-one');
+const sliderPartThree = document.querySelector('.slider-part-two');
 
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
@@ -62,24 +62,27 @@ const shuffleArr = (array) => {
 };
 
 let randomNumbersArr = shuffleArr(numbers);
-console.log(randomNumbersArr);
+// console.log(randomNumbersArr);
 
 // APPEND SLIDER ITEMS
-
+let petImagesOne;
+let petNamesOne;
 function addPetsCardsOne() {
-  // sliderPartOne.innerHTML = '';
-  for (let i = 0; i < 3; i++) {
+  sliderPartOne.innerHTML = '';
+  for (let i = 3; i < 6; i++) {
     const sliderCard = document.createElement('div');
     sliderCard.className = "slider-card"
     sliderPartOne.append(sliderCard);
 
-    const petImage = document.createElement('img');
-    petImage.className = "pet-img"
-    sliderCard.append(petImage);
+    petImagesOne = document.createElement('img');
+    petImagesOne.className = "pet-img"
+    sliderCard.append(petImagesOne);
+    petImagesOne.style.backgroundImage = `url(${(petsDescription[randomNumbersArr[i]].img)})`;
 
-    const petName = document.createElement('div');
-    petName.className = "pet-name paragraph-20"
-    sliderCard.append(petName);
+    petNamesOne = document.createElement('div');
+    petNamesOne.className = "pet-name paragraph-20"
+    sliderCard.append(petNamesOne);
+    petNamesOne.innerHTML = petsDescription[randomNumbersArr[i]].name;
 
     const formLearn = document.createElement('form');
     formLearn.className = "buttons form_learn-more"
@@ -100,13 +103,10 @@ function addPetsCardsOne() {
   };
 };
 
-
-
-
 addPetsCardsOne();
 
 function addPetsCardsTwo() {
-  // sliderPartOne.innerHTML = '';
+  // sliderPartTwo.innerHTML = '';
   for (let i = 0; i < 3; i++) {
     const sliderCard = document.createElement('div');
     sliderCard.className = "slider-card"
@@ -134,22 +134,13 @@ function addPetsCardsTwo() {
     spanLearn.className = "button-text"
     spanLearn.innerHTML = "Learn more"
     buttonLearn.append(spanLearn);
-
   };
 };
 addPetsCardsTwo();
 
 
-// style.backgroundImage = `url(${(levelQuestion[birdId].image)})`;
-function fillCardsContentOnLoad() {
-  for (let i = 0; i < 3; i++) {
-
-
-
-  }
-}
 function addPetsCardsThree() {
-  // sliderPartOne.innerHTML = '';
+  sliderPartThree.innerHTML = '';
   for (let i = 0; i < 3; i++) {
     const sliderCard = document.createElement('div');
     sliderCard.className = "slider-card"
@@ -158,10 +149,12 @@ function addPetsCardsThree() {
     const petImage = document.createElement('img');
     petImage.className = "pet-img"
     sliderCard.append(petImage);
+    petImage.style.backgroundImage = `url(${(petsDescription[randomNumbersArr[i]].img)})`;
 
     const petName = document.createElement('div');
     petName.className = "pet-name paragraph-20"
     sliderCard.append(petName);
+    petName.innerHTML = petsDescription[randomNumbersArr[i]].name
 
     const formLearn = document.createElement('form');
     formLearn.className = "buttons form_learn-more"
@@ -179,41 +172,100 @@ function addPetsCardsThree() {
   };
 };
 addPetsCardsThree();
+let currentArr;
+let theRestNumbersArr;
 
-// function fillPetCards() {
-//   for (let i = 0; i < 9; i++) {
+function fillCardsContentOnLoad(n, arr) {
+  console.log(currentSliderPart);
+  console.log(slider.children[currentSliderPart]);
 
+  // if (currentSliderPart === 0) {
+  // console.log(slider.children[currentSliderPart + 2].previousElementSibling);
 
+  slider.children[n].innerHTML = '';
+  for (let i = 0; i < 3; i++) {
 
-//   }
-// }
+    const sliderCard = document.createElement('div');
+    sliderCard.className = "slider-card"
+    slider.children[n].append(sliderCard);
 
+    const petImage = document.createElement('img');
+    petImage.className = "pet-img"
+    sliderCard.append(petImage);
+    petImage.style.backgroundImage = `url(${(petsDescription[arr[i]].img)})`;
+
+    const petName = document.createElement('div');
+    petName.className = "pet-name paragraph-20"
+    sliderCard.append(petName);
+    petName.innerHTML = petsDescription[arr[i]].name
+
+    const formLearn = document.createElement('form');
+    formLearn.className = "buttons form_learn-more"
+    sliderCard.append(formLearn);
+
+    const buttonLearn = document.createElement('button');
+    buttonLearn.className = "button_learn-more button"
+    formLearn.append(buttonLearn);
+
+    const spanLearn = document.createElement('span');
+    spanLearn.className = "button-text"
+    spanLearn.innerHTML = "Learn more"
+    buttonLearn.append(spanLearn);
+  }
+}
 
 // MOVE SLIDER 
+
+let currentSliderPart = 1;
 let direction;
+console.log(currentSliderPart);
+function makeArray(arr) {
+  currentArr = [];
+  for (var i = 0; i < 3; i++) {
+    currentArr.push(randomNumbersArr[i])
+  }
+  // console.log(currentArr);
+  const s = new Set(currentArr);
+  let newArr = arr.slice(3, 8).filter(e => !s.has(e));
+  return newArr;
+}
+
 arrowRight.addEventListener('click', function () {
+  console.log(currentSliderPart);
   // for (var i = sliderPartOne.children.length; i >= 0; i--) 
+  if (currentSliderPart > 0) {
+    currentSliderPart--
+  } else { currentSliderPart = 2 }
+
+
+  randomNumbersArr = shuffleArr(numbers);
+  theRestNumbersArr = makeArray(randomNumbersArr);
+  fillCardsContentOnLoad(0, theRestNumbersArr);
+
+
+  // addPetsCardsOne();
+  // addPetsCardsThree();
   direction = "right";
   slider.style.transform = 'translate(33.33%';
   slider.style.transitionDuration = "1s"
-console.log(direction);
+  // console.log(randomNumbersArr);
 });
+
 arrowLeft.addEventListener('click', function () {
-  // for (var i = sliderPartOne.children.length; i >= 0; i--)
+  if (currentSliderPart < 2) {
+    currentSliderPart++
+  } else { currentSliderPart = 0 };
+
+  randomNumbersArr = shuffleArr(numbers);
+  theRestNumbersArr = makeArray(randomNumbersArr);
+  fillCardsContentOnLoad(2, theRestNumbersArr);
+
   direction = "left";
   slider.style.transform = 'translate(-33.33%';
   slider.style.transitionDuration = "1s"
-  console.log(direction);
 });
 
-// slider.addEventListener('transitionend', function () {
-//   slider.appendChild(slider.firstElementChild);
-//   slider.style.transition = 'none';
-//   slider.style.transform = 'translate(0)';
-//   setTimeout(function () {
-//     slider.style.transition = 'all 0.4s';
-//   })
-// });
+
 slider.addEventListener('transitionend', function () {
   if (direction === "right") {
     slider.prepend(slider.lastElementChild);
@@ -229,5 +281,6 @@ slider.addEventListener('transitionend', function () {
     setTimeout(function () {
       slider.style.transition = 'all 1s';
     })
+
   }
 });
