44f307a6ada94955a4143968e721f594f7285562
elenaanisimova <anilena232@mail.ru>
2023-04-10 20:33:10 +0800

    feat: add pagination buttons actions

--- shelter/pages/our-pets/script.js
+++ shelter/pages/our-pets/script.js
@@ -2,11 +2,13 @@
 import { petsDescription } from "../../assets/pet-info.js"
 
 const slider = document.querySelector('.slider-visible-wrapper');
-const firstPageButton = document.querySelector('.double-back');
-const previousPageButton = document.querySelector('.back');
-const currentPage = document.querySelector('.page-number');
+const firstPageButton = document.querySelector('.back-button.double-back');
+const previousPageButton = document.querySelector('.back-button.back');
+const currentPage = document.querySelector('.button-text.page-number');
 const nextPageButton = document.querySelector('.next-page');
 const lastPageButton = document.querySelector('.double-next-page');
+const backButtonsText = document.querySelectorAll('.back-buttons-text');
+const nextButtonsText = document.querySelectorAll('.next-buttons-text');
 
 // RANDOMIZER 
 
@@ -16,22 +18,10 @@ const numbers3 = [0, 1, 2, 3, 4, 5, 6, 7];
 const numbers4 = [0, 1, 2, 3, 4, 5, 6, 7];
 const numbers5 = [0, 1, 2, 3, 4, 5, 6, 7];
 const numbers6 = [0, 1, 2, 3, 4, 5, 6, 7];
-const numbers7 = [0, 1, 2, 3, 4, 5, 6, 7];
-const numbers8 = [0, 1, 2, 3, 4, 5, 6, 7];
 
 const shuffleArr = (array) => {
   return array.sort(() => 0.5 - Math.random());
 };
-
-// let randomNumbersArr = shuffleArr(numbers);
-let randomNumbersArr
-
-// let arr3;
-// let arr4;
-// let arr5;
-// let arr6;
-// let arr7;
-// let arr8;
 
 let randomArrays = [];
 function makeRandomArrays() {
@@ -41,8 +31,6 @@ function makeRandomArrays() {
   let arr4 = shuffleArr(numbers4);
   let arr5 = shuffleArr(numbers5);
   let arr6 = shuffleArr(numbers6);
-  let arr7 = shuffleArr(numbers7);
-  let arr8 = shuffleArr(numbers8);
   randomArrays.push(arr1, arr2, arr3, arr4, arr5, arr6)
   return randomArrays
 }
@@ -61,6 +49,7 @@ function appendSliderPages() {
     for (let j = 0; j < 8; j++) {
       const sliderCard = document.createElement('div');
       sliderCard.className = "slider-card";
+      if (j === 0) { sliderCard.className = "slider-card zero"; }
       sliderPage.append(sliderCard);
 
       const petImage = document.createElement('img');
@@ -90,4 +79,135 @@ function appendSliderPages() {
 }
 
 appendSliderPages();
+
+// MOVE SLIDER 
+
+// FIRST PAGE DEFAULT SETTINGS 
+let i = 0;
+slider.children[i].classList.add('visible');
+currentPage.innerHTML = i + 1;
+nextPageButton.classList.add("active-button");
+lastPageButton.classList.add("active-button");
+for (const span of backButtonsText) {
+  span.classList.add("disabled-button-text")
+};
+
+// CHANGE PAGES FUNCTIONS 
+
+function nextPage() {
+  slider.children[i].classList.remove('visible');
+  i++;
+  slider.children[i].classList.add('visible');
+  currentPage.innerHTML = i + 1;
+  if (i >= 5) {
+    lastPageButton.disabled = true;
+    nextPageButton.disabled = true;
+    lastPageButton.classList.remove("active-button");
+    nextPageButton.classList.remove("active-button");
+    for (const span of nextButtonsText) {
+      span.classList.add("disabled-button-text")
+    }
+  }
+  if (i > 0) {
+    firstPageButton.disabled = false;
+    previousPageButton.disabled = false;
+    firstPageButton.classList.add("active-button");
+    previousPageButton.classList.add("active-button");
+    for (const span of backButtonsText) {
+      span.classList.remove("disabled-button-text")
+    }
+  }
+}
+
+nextPageButton.addEventListener('click', function () {
+  nextPage();
+  console.log(i);
+});
+
+function previousPage() {
+  slider.children[i].classList.remove('visible');
+  i--;
+  slider.children[i].classList.add('visible');
+  currentPage.innerHTML = i + 1;
+  if (i < 1) {
+    firstPageButton.disabled = true;
+    previousPageButton.disabled = true;
+    firstPageButton.classList.remove("active-button");
+    previousPageButton.classList.remove("active-button");
+    for (const span of backButtonsText) {
+      span.classList.add("disabled-button-text")
+    }
+  }
+  if (i < 5) {
+    lastPageButton.disabled = false;
+    nextPageButton.disabled = false;
+    lastPageButton.classList.add("active-button");
+    nextPageButton.classList.add("active-button");
+    for (const span of nextButtonsText) {
+      span.classList.remove("disabled-button-text")
+    }
+  }
+}
+
+previousPageButton.addEventListener('click', function () {
+  previousPage();
+  console.log(i);
+});
+
+function lastPage() {
+  slider.children[i].classList.remove('visible');
+  i = 5;
+  slider.children[i].classList.add('visible');
+  currentPage.innerHTML = i + 1;
+  
+  firstPageButton.disabled = false;
+  previousPageButton.disabled = false;
+  firstPageButton.classList.add("active-button");
+  previousPageButton.classList.add("active-button");
+  for (const span of backButtonsText) {
+    span.classList.remove("disabled-button-text")
+  }
+
+  lastPageButton.disabled = true;
+  nextPageButton.disabled = true;
+  lastPageButton.classList.add("active-button");
+  nextPageButton.classList.add("active-button");
+  for (const span of nextButtonsText) {
+    span.classList.add("disabled-button-text")
+  }
+}
+
+
+lastPageButton.addEventListener('click', function () {
+  lastPage()
+  console.log(i);
+});
+
+function firstPage() {
+  slider.children[i].classList.remove('visible');
+  i = 0;
+  slider.children[i].classList.add('visible');
+  currentPage.innerHTML = i + 1;
+
+  firstPageButton.disabled = true;
+  previousPageButton.disabled = true;
+  firstPageButton.classList.add("active-button");
+  previousPageButton.classList.add("active-button");
+  for (const span of backButtonsText) {
+    span.classList.add("disabled-button-text")
+  }
+  lastPageButton.disabled = false;
+  nextPageButton.disabled = false;
+  lastPageButton.classList.add("active-button");
+  nextPageButton.classList.add("active-button");
+  for (const span of nextButtonsText) {
+    span.classList.remove("disabled-button-text")
+  }
+}
+
+firstPageButton.addEventListener('click', function () {
+  firstPage()
+  console.log(i);
+});
+
 
